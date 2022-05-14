bbaec091798f3882e4b5b1587399ce44472a05d0
melakemikru <46749625+melakemikru@users.noreply.github.com>
2022-05-14 13:14:20 +0300

    Add files via upload

---
+++ SportStore/SportStore/data.js
@@ -0,0 +1,76 @@
+module.exports = function () {
+  return {
+    products: [{
+        id: 1,
+        name: "kayak",
+        category: "WaterSports",
+        description: "A boat for one person",
+        price: 270
+      },
+      {
+        id: 2,
+        name: "kayak",
+        category: "WaterSports",
+        description: "A boat for one person",
+        price: 270
+      },
+      {
+        id: 3,
+        name: "kayak",
+        category: "WaterSports",
+        description: "A boat for one person",
+        price: 270
+      },
+      {
+        id: 4,
+        name: "kayak",
+        category: "WaterSports",
+        description: "A boat for one person",
+        price: 270
+      },
+      {
+        id: 5,
+        name: "kayak",
+        category: "WaterSports",
+        description: "A boat for one person",
+        price: 270
+      },
+      {
+        id: 6,
+        name: "kayak",
+        category: "WaterSports",
+        description: "A boat for one person",
+        price: 270
+      },
+      {
+        id: 7,
+        name: "kayak",
+        category: "WaterSports",
+        description: "A boat for one person",
+        price: 270
+      },
+      {
+        id: 8,
+        name: "kayak",
+        category: "WaterSports",
+        description: "A boat for one person",
+        price: 270
+      },
+      {
+        id: 9,
+        name: "kayak",
+        category: "WaterSports",
+        description: "A boat for one person",
+        price: 270
+      },
+      {
+        id: 10,
+        name: "kayak",
+        category: "WaterSports",
+        description: "A boat for one person",
+        price: 270
+      }
+    ],
+    orders: []
+  }
+}
